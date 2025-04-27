9ac2aab2ccbd1eb691922276522abc8242467e25
sdsdsL <122241505+sdsdsL@users.noreply.github.com>
2025-04-27 23:43:06 +0300

    Update 500.tsx
    переведено на русский, честно скрадено у vepurovk.xyz
    
    Signed-off-by: sdsdsL <122241505+sdsdsL@users.noreply.github.com>

--- website/src/pages/500.tsx
+++ website/src/pages/500.tsx
@@ -4,13 +4,13 @@ export default function Custom500() {
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-900 dark:text-primary-100">
-            500
+            Ошибка сервера
           </h1>
           <p className="mb-4 text-3xl tracking-tight font-bold text-neutral-900 md:text-4xl dark:text-white">
-            Internal Server Error.
+            Ой блять! возможно что-то наебнулось.. Возможно мы уже вкурсе этой проблемы и уже работаем над её устранением. Пожалуйста, попробуйте зайти позже.
           </p>
           <p className="mb-4 text-lg font-light text-neutral-800 dark:text-neutral-100">
-            We are already working to solve the problem.
+            Error 500 - ёбаный рот.
           </p>
         </div>
       </div>
